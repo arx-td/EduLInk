212c0821c65ef108c82a2d0540d82ed1b79d7b1c
arx_td <arxthedesigner2021@gmail.com>
2025-08-20 16:06:56 +0500

    add all files

---
+++ backend/controllers/class-controller.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+
+const Sclass = require('../models/sclassSchema.js');
+const Student = require('../models/studentSchema.js');
+const Subject = require('../models/subjectSchema.js');
+const Teacher = require('../models/teacherSchema.js');
+
+const sclassCreate = async (req, res) => {
+    try {
+        const sclass = new Sclass({
+            sclassName: req.body.sclassName,
+            school: req.body.adminID
+        });
+
+        const existingSclassByName = await Sclass.findOne({
+            sclassName: req.body.sclassName,
+            school: req.body.adminID
+        });
+
+        if (existingSclassByName) {
+            res.send({ message: 'Sorry this class name already exists' });
+        }
+        else {
+            const result = await sclass.save();
+            res.send(result);
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const sclassList = async (req, res) => {
+    try {
+        let sclasses = await Sclass.find({ school: req.params.id })
+        if (sclasses.length > 0) {
+            res.send(sclasses)
+        } else {
+            res.send({ message: "No sclasses found" });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+const getSclassDetail = async (req, res) => {
+    try {
+        let sclass = await Sclass.findById(req.params.id);
+        if (sclass) {
+            sclass = await sclass.populate("school", "schoolName")
+            res.send(sclass);
+        }
+        else {
+            res.send({ message: "No class found" });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+const getSclassStudents = async (req, res) => {
+    try {
+        let students = await Student.find({ sclassName: req.params.id })
+        if (students.length > 0) {
+            let modifiedStudents = students.map((student) => {
+                return { ...student._doc, password: undefined };
+            });
+            res.send(modifiedStudents);
+        } else {
+            res.send({ message: "No students found" });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+const deleteSclass = async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const deletedClass = await Sclass.findByIdAndDelete(req.params.id, { session });
+        if (!deletedClass) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(404).json({ message: "Class not found" });
+        }
+
+        await Student.deleteMany({ school: req.params.id }, { session });
+        await Subject.deleteMany({ school: req.params.id }, { session });
+        await Teacher.deleteMany({ school: req.params.id }, { session });
+
+        await session.commitTransaction();
+        session.endSession();
+        res.status(200).json(deletedClass);
+
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        console.error("Error deleting class:", error);
+        res.status(500).json({ message: "An error occurred while deleting the class." });
+    }
+};
+
+
+const deleteSclasses = async (req, res) => {
+    try {
+        const deletedClasses = await Sclass.deleteMany({ school: req.params.id });
+        if (deletedClasses.deletedCount === 0) {
+            return res.send({ message: "No classes found to delete" });
+        }
+        const deletedStudents = await Student.deleteMany({ school: req.params.id });
+        const deletedSubjects = await Subject.deleteMany({ school: req.params.id });
+        const deletedTeachers = await Teacher.deleteMany({ school: req.params.id });
+        res.send(deletedClasses);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+module.exports = { sclassCreate, sclassList, deleteSclass, deleteSclasses, getSclassDetail, getSclassStudents };
